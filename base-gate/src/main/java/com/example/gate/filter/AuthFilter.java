e69e297f1d38ce541699018a7b5bb6349b73a1ec
Zh <2499697471@qq.com>
2025-06-04 00:55:38 +0800

    fix: 完善sentinel 和 zipkin

--- base-gate/src/main/java/com/example/gate/filter/AuthFilter.java
+++ base-gate/src/main/java/com/example/gate/filter/AuthFilter.java
@@ -3,7 +3,6 @@ package com.example.gate.filter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -17,10 +16,10 @@ public class AuthFilter implements GlobalFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String token = exchange.getRequest().getHeaders().getFirst("Authorization");
         // 如果 Token 不存在或未以 Bearer 开头，返回 401 UNAUTHORIZED。
-        if (token == null || !token.startsWith("Bearer ")) {
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
-        }
+//        if (token == null || !token.startsWith("Bearer ")) {
+//            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+//            return exchange.getResponse().setComplete();
+//        }
         return chain.filter(exchange);
     }
 
